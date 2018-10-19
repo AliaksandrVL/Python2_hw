bd2b302688332cf666c906705dd8c9bf496c317a
AliaksandrVL <litvinuk2@gmail.com>
2018-10-19 08:23:59 +0300

    Prepare for homework 4

--- Python2_hw3/client.py
+++ Python2_hw3/client.py
@@ -2,33 +2,59 @@ import json
 import time
 from config import get_client_socket
 
-AUTHORIZED = False
+USERNAME = ""
+
+def send_data_authenticate(s, client_msg):
+
+    add_msg = {
+        "user": {
+            "username": input("User name: "),
+            "password": int(input("Password: "))
+        }
+    }
+
+    client_msg.update(add_msg)
+    s.send(json.dumps(client_msg).encode(client_msg["encoding"]))
+
+def send_data_msg(s, client_msg):
+
+    add_msg = {
+        "user": {
+            "to": input("To: "),
+            "from": USERNAME,
+            "message": input("Message: ")
+        }
+    }
+
+    client_msg.update(add_msg)
+    s.send(json.dumps(client_msg).encode(client_msg["encoding"]))
+
+def send_data_quit(s, client_msg):
+    s.send(json.dumps(client_msg).encode(client_msg["encoding"]))
+    exit()
+
+def send_client_msg(s, action):
+
+    client_msg = {
+        "action": f"{action}",
+        "time": int(time.time()),
+        "encoding": "utf-8",
+    }
+
+    if action == "authenticate":
+        send_data_authenticate(s, client_msg)
+    elif action == "msg":
+        send_data_msg(s, client_msg)
+    elif action == "quit":
+        send_data_quit(s, client_msg)
 
 def send_data(s):
 
-    if not AUTHORIZED:
-        client_msg = {
-            "action": "authenticate",
-            "time": int(time.time()),
-            "encoding": "utf-8",
-            "user": {
-                "username": input("User name: "),
-                "password": int(input("Password: "))
-            }
-        }
+    if USERNAME == "":
+        send_client_msg(s, "authenticate")
     else:
         action = input("Action: ")
-
-        if action == "quit":
-            client_msg = {
-                "action": f"{action}",
-                "time": int(time.time()),
-                "encoding": "utf-8"
-            }
-            s.send(json.dumps(client_msg).encode(client_msg["encoding"]))
-            exit()
-
-    s.send(json.dumps(client_msg).encode(client_msg["encoding"]))
+        send_client_msg(s, action)
 
 if __name__ == "__main__":
     s = get_client_socket()
@@ -38,7 +64,7 @@ if __name__ == "__main__":
 
         server_response = json.loads(s.recv(10240).decode("utf-8"))
 
-        if not AUTHORIZED and server_response["response"] == "200":
-            AUTHORIZED = True
+        if USERNAME == "" and server_response["response"] == "200":
+            USERNAME = "Admin"
 
         print(server_response["alert"])
